d8f600ae599d3c6634fbae48ab8f77ac6f69019e
Zohid <777aaauz@gmail.com>
2023-11-17 13:17:40 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import { config } from 'dotenv'
+import cors from 'cors'
+
+import authRoute from './routes/auth.js'
+
+const app = express()
+
+config()
+const PORT = process.env.PORT || 3000
+
+//Middleware
+app.use(cors())
+app.use(express.json())
+
+//Routes
+app.use('/api/auth', authRoute)
+
+async function start() {
+  try {
+    const conn = await mongoose.connect(
+      process.env.MONGO_URL, {
+        useNewUrlParser: true
+      }
+    )
+    app.listen(PORT, () => {
+      console.log(`Server started on port: ${PORT}`)
+    })
+    console.log(`MongoDB connected to: ${conn.connection.host}`)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
+
+
